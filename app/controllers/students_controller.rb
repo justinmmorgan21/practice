9ae4501a4a507ebc5fca8610a61e84c39569b6fc
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-19 13:39:35 -0500

    added Student routes and actions for update and delete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,7 @@ class StudentsController < ApplicationController
       email: params[:email],
     }
     end
+    
     def create
       @student = Student.new(student_params)
       if @student.save
@@ -14,13 +15,31 @@ class StudentsController < ApplicationController
         render json: {error: @student.errors.full_messages}, status: :unprocessable_entity
       end
     end
+    
     def show
-      @student = Student.find_by(params[:id])
+      @student = Student.find_by(id: params[:id])
       render :show
     end
     def index
       @students = Student.all
       render :index
+    end
+    
+    def update
+      @student = Student.find_by(id: params[:id])
+      @student.name = params[:name] || @student.name
+      @student.dob = params[:dob] || @student.dob
+      @student.email = params[:email] || @student.email
+      if @student.save
+        render :show
+      else
+        render json: {error: @student.errors.full_messages}, status: :unprocessable_entity
+      end
+    end
+    def destroy
+      @student = Student.find_by(id: params[:id])
+      @student.destroy
+      render json: {message: "#{@student.name} deleted."}
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   post "/students" => "students#create"
   get "/students/:id" => "students#show"
   get "/students" => "students#index"
-
+  patch "/students/:id" => "students#update"
+  delete "/students/:id" => "students#destroy"
 end
